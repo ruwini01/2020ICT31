8812f189f35b87644dd078211a831b049185a779
Ruwini Tharanga <128955270+ruwini01@users.noreply.github.com>
2024-11-19 09:40:16 +0000

    Header file created

---
+++ myapp/src/components/Header.js
@@ -0,0 +1,15 @@
+export default function Header(){
+    return (
+        <div>
+            <h1 className='header'>Student Infromation Portal</h1>
+            <hr/>
+            <div className='buttons'>
+                <p>fontSize:</p>
+                <button>S</button>
+                <button>M</button>
+                <button>L</button>
+            </div>
+            <hr/>
+        </div>
+    );
+}
