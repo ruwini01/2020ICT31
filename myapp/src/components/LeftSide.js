b9b308fbdcb11cb2a0faffa00ef4cc047226fe70
Ruwini Tharanga <128955270+ruwini01@users.noreply.github.com>
2024-11-19 17:07:56 +0000

    LeftSide Table Created with view button

--- myapp/src/components/LeftSide.js
+++ myapp/src/components/LeftSide.js
@@ -1,5 +1,25 @@
-export default function LeftSide(){
-    return <div>
-            
-        </div>
+import React from 'react'
+
+function LeftSide(props){
+    return <table border={1}>
+        <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Course</th>
+            <th>Country</th>
+            <th>Profile</th>
+        </tr>
+        {props.students.map((student)=>(
+            <tr key={student.studentId}>
+                <td>{student.firstName}</td>
+                <td>{student.lastName}</td>
+                <td>{student.course}</td>
+                <td>{student.address.country}</td>
+                <td><button onClick={()=>props.setStudent(student)}>View</button></td>
+            </tr>
+            )
+        )}
+    </table>
 }
+
+export default LeftSide;
